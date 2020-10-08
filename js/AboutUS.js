285508a7f39c1c66693928db746fcf114557c738
Riti <rrdobariya27@gmail.com>
2020-10-08 21:08:34 +0530

    edited AboutUs.html

--- js/AboutUS.js
+++ js/AboutUS.js
@@ -3,3 +3,10 @@ $(document).ready(function(){
         $(window).scrollTop(0);
     });
 });
+
+
+window.console = window.console || function(t) {};
+  if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+  }
+
