07900299476b7ee5684ea20b63e1bb45362a7378
Mit-Patel <patelmit2012@gmail.com>
2020-11-24 20:45:02 +0530

    merged css files, deleted extra files, created master navbar, footer, and page-header, etc.

--- js/index.js
+++ js/index.js
@@ -1,78 +1,79 @@
-function checkAndActiveAboutUs() {
-  if(window.location.hash === '#abt-us') {
-    var headerLinks = document.querySelectorAll('.header-right-link');
-    headerLinks.forEach(function(x) {
-      x.classList.remove("current-active");
-    })
-    document.getElementById("about-us").classList.add("current-active");
-  }
+function checkAndActiveAboutUs(document) {
+    if (window.location.hash === '#abt-us') {
+        var headerLinks = document.querySelectorAll('.header-right-link');
+        headerLinks.forEach(function(x) {
+            x.classList.remove("current-active");
+        });
+        let aboutus = document.getElementById("about-us");
+        aboutus.classList.add("current-active");
+    }
 }
 
 checkAndActiveAboutUs();
 
 $(function($) {
-  "use strict";
-  // Smooth scroll for the navigation menu and links with .scrollto classes
-  var scrolltoOffset = $('#abt-us').outerHeight() - 1;
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      e.preventDefault();
-      var target = $(this.hash);
-      if (target.length) {
+    "use strict";
+    // Smooth scroll for the navigation menu and links with .scrollto classes
+    var scrolltoOffset = $('#abt-us').outerHeight() - 1;
+    $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            e.preventDefault();
+            var target = $(this.hash);
+            if (target.length) {
 
-        var scrollto = target.offset().top - scrolltoOffset;
-        if ($(this).attr("href") == '#abt-us') {
-          scrollto = 0;
+                var scrollto = target.offset().top - scrolltoOffset;
+                if ($(this).attr("href") == '#abt-us') {
+                    scrollto = 0;
+                }
+
+                $('html, body').animate({
+                    scrollTop: scrollto
+                }, 1500, 'easeInOutExpo');
+
+                if ($(this).parents('.nav-menu, .mobile-nav').length) {
+                    $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+                    $(this).closest('li').addClass('active');
+                }
+
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+                    $('.mobile-nav-overly').fadeOut();
+                }
+                return false;
+            }
         }
-
+    });
+    // Activate smooth scroll on page load with hash links in the url
+    $(document).ready(function() {
+        window.addEventListener('hashchange', checkAndActiveAboutUs)
+        if (window.location.hash) {
+            var initial_nav = window.location.hash;
+            if ($(initial_nav).length) {
+                var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+                $('html, body').animate({
+                    scrollTop: scrollto
+                }, 1500, 'easeInOutExpo');
+            }
+        }
+    });
+    // Stick the header at top on scroll
+    // $("#header").sticky({
+    //   topSpacing: 0,
+    //   zIndex: '50'
+    // });
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function() {
         $('html, body').animate({
-          scrollTop: scrollto
+            scrollTop: 0
         }, 1500, 'easeInOutExpo');
-
-        if ($(this).parents('.nav-menu, .mobile-nav').length) {
-          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-          $(this).closest('li').addClass('active');
-        }
-
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
-        }
         return false;
-      }
-    }
-  });
-  // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function() {
-    window.addEventListener('hashchange', checkAndActiveAboutUs)
-    if (window.location.hash) {
-      var initial_nav = window.location.hash;
-      if ($(initial_nav).length) {
-        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-      }
-    }
-  });
-  // Stick the header at top on scroll
-  // $("#header").sticky({
-  //   topSpacing: 0,
-  //   zIndex: '50'
-  // });
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
+    });
 });
--- js/script.js
+++ js/script.js
@@ -71,3 +71,120 @@ function createProjectList() {
     });
     $(".project-card-list>.row").append(text);
 }
+
+
+/*
+ * This function will create the Navigation Bar in the page wherever it will be called
+ */
+
+function createNavBar(activePage) {
+    let text = `    
+        <div class="header-left">
+            <img src="./imgs/DSC_LOGO.png">
+        </div>
+        <input type="checkbox" class="nav-toggle" id="nav-toggle">
+        <div class="header-right">
+            <a href="./index.html" id="home" class="header-right-link ` + activePage[0] + `"><li class="header-right-link">Home</li></a>
+            <a href="./index.html#abt-us" id="about-us" class="header-right-link ` + activePage[1] + `"><li class="header-right-link">About Us</li></a>
+            <a href="./projects.html" class="header-right-link ` + activePage[2] + `"><li class="header-right-link">Projects</li></a>
+            <a href="./teams.html" class="header-right-link ` + activePage[3] + `"><li class="header-right-link">Teams</li></a>
+            <a href="./gallery.html" class="header-right-link ` + activePage[4] + `"><li class="header-right-link">Gallery</li></a>
+            <a href="./contactUs.html" class="header-right-link ` + activePage[5] + `"><li class="header-right-link">Contact Us</li></a>
+        </div>
+        <label for="nav-toggle" class="nav-toggle-label">
+            <i class="fa fa-bars" style="font-size:28px;color: white"></i>
+        </label>
+    `;
+
+    $("header").append(text);
+
+    $("head").append(`<link rel="icon" type="image/png" href="imgs/favicon.png">`);
+
+    $("#about-us").click((e) => {
+        if ($("#home").hasClass("current-active"))
+            $("#home").removeClass("current-active");
+        $("#about-us").addClass("current-active");
+    });
+
+    if (window.location.hash.substr(1) === "abt-us") {
+        if ($("#home").hasClass("current-active"))
+            $("#home").removeClass("current-active");
+        $("#about-us").addClass("current-active");
+    }
+}
+
+/*
+ * This function will create the Footer in the page wherever it will be called
+ */
+
+function createFooter() {
+    let text = `
+    <div class="container">
+        <div class="row" style="margin-left: 10%;margin-right: 10%;">
+            <section id="quick" class="col-md-offset-2 col-md-3 col-sm-4 col-xs-12">
+                <p>Quick Links</p>
+                <a href="https://www.rsu.ac.in" target="_blank">Rashtriya Raksha University</a><br>
+                <a href="https://docs.google.com/document/d/1DRbb0VUGORYI9epY3INSEfkWT_SO7EKVQTsieeA3MR4/edit" target="_blank">Code of Conduct</a><br>
+                <a href="https://docs.google.com/document/d/1TDQdYO0Dm0osDrFzyqfe1MjemHMJMdpCQFZVUI4Y-qk/edit" target="_blank">FAQs</a><br>
+            </section>
+            <section id="socia" class="col-md-3 col-sm-4 col-xs-12">
+                <p>Social Links</p>
+                <a href="https://dsc.community.dev/raksha-shakti-university/" target="_blank">Developer Student Club</a><br>
+                <a href="https://facebook.com/dsc.rsu.7" target="_blank">Facebook</a><br>
+                <a href="https://twitter.com/Dsc__Rsu" target="_blank">Twitter</a><br>
+                <a href="https://www.linkedin.com/in/dsc-rsu-7b53011b8/" target="_blank">Linkedin</a><br>
+                <a href="https://www.instagram.com/dscrsu/" target="_blank">Instagram</a><br>
+            </section>
+            <section id="scroll" class="col-md-3 col-sm-4 col-xs-12">
+                <p>Scroll To</p>
+                <a href="index.html#abt-us">About Us</a><br>
+                <a href="teams.html">Team</a><br>
+                <a href="projects.html">Projects</a><br>
+                <a href="contactus.html">Contact Us</a><br>
+            </section>
+        </div>
+        <hr style="border-top: 1px solid #f8f9fa;">
+        <div align="center" class="copyright"> Copyright &copy; Developer Student Club - <a href="https://rsu.ac.in" target="_blank" style="color: yellow">Rashtriya Raksha University</a></div>
+    </div>
+    `;
+
+    $("footer").append(text);
+}
+
+/*
+ * This function will create the Header of the page wherever it will be called
+ */
+
+function createPageHeader(title, subtitle, image, side = 0) {
+    let text = `
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <span>` + title + `</span> <br>
+            <p>` + subtitle + `</p>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <img class="img-responsive" src="` + image + `" alt="` + title + ` Illustration">
+        </div>
+    </div>
+    `;
+    if (side === 1) {
+        text = `
+        <div class="row">            
+            <div class="col-md-6 col-sm-12" style="top: none;transform: none;">
+                <img class="img-responsive" src="` + image + `" alt="` + title + ` Illustration">
+            </div>
+            <div class="col-md-6 col-sm-12" style="top: 50%;transform: translate(0%,50%);">
+                <span>` + title + `</span> <br>
+                <p>` + subtitle + `</p>
+            </div>
+        </div>
+        `;
+    }
+    $(".project-header").append(text);
+
+    let margin = $(".header-nav").outerHeight();
+    $(".project-header").css({
+        "margin-top": margin,
+        "height": "calc(100vh - " + margin + "px)"
+    });
+}
